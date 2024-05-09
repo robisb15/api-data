08bfc14764b269df0b3985de2d30dd851631914f
robisb15 <robiaulia.k@gmail.com>
2024-05-09 15:52:57 +0700

    vercel-deploy

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 // Middleware
-app.use(bodyParser.json());
+
 
 // Dummy data
 let products = [
